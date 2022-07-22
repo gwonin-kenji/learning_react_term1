25d995a1d0d02347d694c35e18be35fb374f339d
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-22 13:33:57 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import ReactDom from "react-dom";
+
+import App from "./App";
+
+ReactDom.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
+
+// ver18以降の renderの方法
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
