a45b9683c28dd3c73dc860429236f681abeecc5f
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-23 11:48:23 +0900

    propsを分割代入(unpacking)

--- src/components/ColofulMessage.jsx
+++ src/components/ColofulMessage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const ColofulMessage = (props) => {
   console.log(props);
 
+  const { color, children } = props;
+
   const contentStyle = {
-    color: props.color,
+    color: color,
     fontSize: "18px"
   };
 
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColofulMessage;
